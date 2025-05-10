1d4f283cb80cf3f8803a95141b7ad157f1324642
Armaan <mohammadarmaan533@gmail.com>
2025-05-10 20:08:06 +0530

    lexer finished

--- src/lexer.py
+++ src/lexer.py
@@ -3,36 +3,7 @@ from ply import lex
 
 # Lexer for C or C-like languages
 def tokenize(data):
-    # List of token names. This is always required
-    tokens = (
-        'IDENTIFIER',
-        'NUMBER',
-        'PLUS',
-        'MINUS',
-        'TIMES',
-        'DIVIDE',
-        'LPAREN',
-        'RPAREN',
-        'EQUALS',
-        'SEMICOLON',
-        'COMMA',
-        'LBRACE',
-        'RBRACE'
-    )
-
-    # Regular expression rules for simple tokens
-    t_PLUS = r'\+'
-    t_MINUS = r'-'
-    t_TIMES = r'\*'
-    t_DIVIDE = r'/'
-    t_LPAREN = r'\('
-    t_RPAREN = r'\)'
-    t_LBRACE = r'\{'
-    t_RBRACE = r'\}'
-    t_EQUALS = r'='
-    t_SEMICOLON = r';'
-    t_COMMA = r','
-
+    
     reserved = {
         'alignas': 'ALIGNAS',
         'alignof': 'ALIGNOF',
@@ -78,7 +49,116 @@ def tokenize(data):
         'while': 'WHILE'
     }
 
-    def t_ID(t):
+
+    # List of token names. This is always required
+    tokens = (
+        'IDENTIFIER',
+        'NUMBER',
+        'STRINGLITERAL',
+        'CHARLITERAL',
+        'PREPROCESSOR',
+        'EQUALS',
+        'PLUS',
+        'MINUS',
+        'TIMES',
+        'DIVIDE',
+        'MODULUS',
+        'INCREMENT',
+        'DECREMENT',
+        'PLUSEQUALS',
+        'MINUSEQUALS',
+        'TIMESEQUALS',
+        'DIVIDEEQUALS',
+        'MODULUSEQUALS',
+        'ANDEQUALS',
+        'OREQUALS',
+        'XOREQUALS',
+        'LEFTSHIFTEQUALS',
+        'RIGHTSHIFTEQUALS',
+        'ISEQUALS',
+        'ISNOTEQUALS',
+        'ISLESSTHAN',
+        'ISGREATERTHAN',
+        'ISLESSTHANEQUAL',
+        'ISGREATERTHANEQUAL',
+        'AND',
+        'OR',
+        'NOT',
+        'BITAND',
+        'BITOR',
+        'BITXOR',
+        'BITNOT',
+        'BITSHIFTLEFT',
+        'BITSHIFTRIGHT',
+        'CONDITIONAL',
+        'ADDRESSOF',
+        'DEREFERNCETO',
+        'ARROW',
+        'SEMICOLON',
+        'COLON',
+        'COMMA',
+        'DOT',
+        'LPAREN',
+        'RPAREN',
+        'LBRACE',
+        'RBRACE',
+        'LSQUARE',
+        'RSQUARE'
+    ) + tuple(reserved.values())  # Add reserved words to tokens
+
+    # Regular expression rules for simple tokens
+    t_PREPROCESSOR = r'\#\s*[a-zA-Z_][a-zA-Z0-9_]*'
+    t_STRINGLITERAL = r'"([^\\\"]|\\.)*"'
+    t_CHARLITERAL = r"'([^\\']|\\.)*'"
+    t_EQUALS = r'='
+    t_PLUS = r'\+'
+    t_MINUS = r'-'
+    t_TIMES = r'\*'
+    t_DIVIDE = r'/'
+    t_MODULUS = r'%'
+    t_INCREMENT = r'\+\+'
+    t_DECREMENT = r'--'
+    t_PLUSEQUALS = r'\+='
+    t_MINUSEQUALS = r'-='
+    t_TIMESEQUALS = r'\*='
+    t_DIVIDEEQUALS = r'/='
+    t_MODULUSEQUALS = r'%='
+    t_ANDEQUALS = r'&='
+    t_OREQUALS = r'\|='
+    t_XOREQUALS = r'\^='
+    t_LEFTSHIFTEQUALS = r'<<='
+    t_RIGHTSHIFTEQUALS = r'>>='
+    t_ISEQUALS = r'=='
+    t_ISNOTEQUALS = r'!='
+    t_ISLESSTHAN = r'<'
+    t_ISGREATERTHAN = r'>'
+    t_ISLESSTHANEQUAL = r'<='
+    t_ISGREATERTHANEQUAL = r'>='
+    t_AND = r'&&'
+    t_OR = r'\|\|'
+    t_NOT = r'!'
+    t_BITAND = r'&'
+    t_BITOR = r'\|'
+    t_BITXOR = r'\^'
+    t_BITNOT = r'~'
+    t_BITSHIFTLEFT = r'<<'
+    t_BITSHIFTRIGHT = r'>>'
+    t_CONDITIONAL = r'\?'
+    t_ADDRESSOF = r'&'
+    t_DEREFERNCETO = r'\*'
+    t_ARROW = r'->'
+    t_SEMICOLON = r';'
+    t_COLON = r':'
+    t_COMMA = r','
+    t_DOT = r'\.'
+    t_LPAREN = r'\('
+    t_RPAREN = r'\)'
+    t_LBRACE = r'\{'
+    t_RBRACE = r'\}'
+    t_LSQUARE = r'\['
+    t_RSQUARE = r'\]'
+
+    def t_IDENTIFIER(t):
         r'[A-Za-z_][A-Za-z0-9_]*'
         t.type = reserved.get(t.value, 'IDENTIFIER')  # Check for reserved words
         return t
--- src/main.py
+++ src/main.py
@@ -1,5 +1,40 @@
 import lexer
 
 if __name__ == "__main__":
-    lexer = lexer.tokenize("int main() { return 0; }")      # Insert the code here
-    print(lexer)
+    lexer = lexer.tokenize("""
+    #include <stdio.h>
+    int main(){
+	int i;
+	scanf("%d",&i);
+	switch(i)
+	{
+		case(1):{
+			printf("one");
+			break;
+		}
+		case(2):{
+			printf("two");
+			break;
+		}
+		case(3):{
+			printf("three");
+			break;
+		}
+		case(4):{
+			printf("four");
+			break;
+		}
+		case(5):{
+			printf("five");
+			break;
+		}
+		case(6):{
+			printf("six");
+			break;
+		}
+		default:printf("Please enter value in range of 1-6");
+	}
+}
+""")      # Insert the code here
+    for tokens in lexer:
+        print(tokens)
